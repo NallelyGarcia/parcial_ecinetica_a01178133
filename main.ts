7365c56f1a0fd81b697f11e205d0c48f2d7c9e17
NallelyGarcia <70342524+NallelyGarcia@users.noreply.github.com>
2020-09-17 13:39:08 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Ec =")
+    velocidad_cuadrada = 15 ** 2
+    masa_multiplicada = 17.78 * velocidad_cuadrada
+    total = masa_multiplicada / 2
+    basic.showNumber(total)
+})
+let total = 0
+let masa_multiplicada = 0
+let velocidad_cuadrada = 0
+basic.showString("Nallely Garcia A01178133")
+let masa = 17.78
+let veelocidad = 15
